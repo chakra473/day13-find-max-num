9f290f5280cb4541478e2ea1ad8db6e23b12df6e
chakravarthy <m.chakra473@gmail.com>
2022-03-22 18:34:38 +0530

    added [extend the max method to take more than three parameters] to uc4

---
+++ GenericClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day13_find_max
+{
+    internal class GenericClass<T> where T : IComparable
+    {
+        public T[] value;
+        public GenericClass(T[] value)
+        {
+            this.value = value;
+        }
+        public T[] sort(T[] values)
+        {
+            Array.Sort(values);
+            return value;
+        }
+        public T MaxValue(params T[] values)
+        {
+            var sorted=sort(values);
+            return sorted[^1];
+        }
+        public T MaxMethod()
+        {
+            var max=MaxValue(this.value);
+            return max;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,74 +7,32 @@ namespace day13_find_max
         static void Main(string[] args)
         {
             Console.WriteLine("welcome to find the max number");
-            Console.WriteLine("enter three numbers");
-            int a=int.Parse(Console.ReadLine());
-            int b=int.Parse(Console.ReadLine());
-            int c=int.Parse(Console.ReadLine());
-            Console.WriteLine("the maximum number among three number is :");
-            Console.WriteLine(FindMax(a, b, c));
-            Console.WriteLine("enter three double numbers");
-            double d=double.Parse(Console.ReadLine());
-            double e=double.Parse(Console.ReadLine());
-            double f=double.Parse(Console.ReadLine());
-            Console.WriteLine("the maximum double number among three number is :");
-            Console.WriteLine(FindMaxDouble(d, e, f));
-            Console.WriteLine("the three string");
-            string g=Console.ReadLine();
-            string h=Console.ReadLine();
-            string i=Console.ReadLine();
-            Console.WriteLine("the maximum among three string is :");
-            Console.WriteLine(FindMaxString(i, g, h));
+            Console.WriteLine("enter the values");
+            int[] arr = new int[5];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] =int.Parse(Console.ReadLine());
+            }
+            GenericClass<int> intMax=new GenericClass<int>(arr);
+            Console.WriteLine(intMax.MaxMethod());
+            Console.WriteLine("enter the float values");
+            float[] arrFloat=new float[5];
+            for(int i = 0; i < arrFloat.Length; i++)
+            {
+                arrFloat[i]= float.Parse(Console.ReadLine());
+            }
+            GenericClass<float> floatMax=new GenericClass<float>(arrFloat);
+            Console.WriteLine(floatMax.MaxMethod());
+            Console.WriteLine("enter the string values");
+            string [] arrString=new string[5];
+            for(int i=0; i < arrString.Length; i++)
+            {
+                arrString[i]= Console.ReadLine();
+            }
+            GenericClass<string> stringMax=new GenericClass<string>(arrString);
+            Console.WriteLine(stringMax.MaxMethod());
 
-
         }
-        public static int FindMax(int a,int b,int c)
-        {
-            if(a.CompareTo(b) > 0&& a.CompareTo(c) > 0||a.CompareTo(b)>=0&&a.CompareTo(c)>0||a.CompareTo(b)>0&&a.CompareTo(c)>=0)
-            {
-                return a;
-            }
-            else if(b.CompareTo(a) > 0 && b.CompareTo(c) > 0 || b.CompareTo(a) >= 0 && b.CompareTo(c) > 0 || b.CompareTo(a) > 0 && b.CompareTo(c) >= 0)
-            {
-                return b;
-            }
-            else if(c.CompareTo(b) > 0 && c.CompareTo(a) > 0 || c.CompareTo(b) >= 0 && c.CompareTo(a) > 0 || c.CompareTo(b) > 0 && c.CompareTo(a) >= 0)
-            {
-                return c;
-            }
-            return a;
-        }
-        public static double FindMaxDouble(double a, double b, double c)
-        {
-            if (a.CompareTo(b) > 0 && a.CompareTo(c) > 0 || a.CompareTo(b) >= 0 && a.CompareTo(c) > 0 || a.CompareTo(b) > 0 && a.CompareTo(c) >= 0)
-            {
-                return a;
-            }
-            else if (b.CompareTo(a) > 0 && b.CompareTo(c) > 0 || b.CompareTo(a) >= 0 && b.CompareTo(c) > 0 || b.CompareTo(a) > 0 && b.CompareTo(c) >= 0)
-            {
-                return b;
-            }
-            else if (c.CompareTo(b) > 0 && c.CompareTo(a) > 0 || c.CompareTo(b) >= 0 && c.CompareTo(a) > 0 || c.CompareTo(b) > 0 && c.CompareTo(a) >= 0)
-            {
-                return c;
-            }
-            return a;
-        }
-        public static string FindMaxString(string a, string b, string c)
-        {
-            if (a.CompareTo(b) > 0 && a.CompareTo(c) > 0 || a.CompareTo(b) >= 0 && a.CompareTo(c) > 0 || a.CompareTo(b) > 0 && a.CompareTo(c) >= 0)
-            {
-                return a;
-            }
-            else if (b.CompareTo(a) > 0 && b.CompareTo(c) > 0 || b.CompareTo(a) >= 0 && b.CompareTo(c) > 0 || b.CompareTo(a) > 0 && b.CompareTo(c) >= 0)
-            {
-                return b;
-            }
-            else if (c.CompareTo(b) > 0 && c.CompareTo(a) > 0 || c.CompareTo(b) >= 0 && c.CompareTo(a) > 0 || c.CompareTo(b) > 0 && c.CompareTo(a) >= 0)
-            {
-                return c;
-            }
-            return a;
-        }
+       
     }
 }
