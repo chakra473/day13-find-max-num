c4d699a614e034c35f4f382d3fbcd3fb8aa4303a
chakravarthy <m.chakra473@gmail.com>
2022-03-22 16:17:22 +0530

    added [find max int among three numbers] to uc1

--- Program.cs
+++ Program.cs
@@ -7,6 +7,28 @@ namespace day13_find_max
         static void Main(string[] args)
         {
             Console.WriteLine("welcome to find the max number");
+            Console.WriteLine("enter three numbers");
+            int a=int.Parse(Console.ReadLine());
+            int b=int.Parse(Console.ReadLine());
+            int c=int.Parse(Console.ReadLine());
+            Console.WriteLine("the maximum number among three number is :");
+            Console.WriteLine(FindMax(a, b, c));
+        }
+        public static int FindMax(int a,int b,int c)
+        {
+            if(a.CompareTo(b) > 0&& a.CompareTo(c) > 0||a.CompareTo(b)>=0&&a.CompareTo(c)>0||a.CompareTo(b)>0&&a.CompareTo(c)>=0)
+            {
+                return a;
+            }
+            else if(b.CompareTo(a) > 0 && b.CompareTo(c) > 0 || b.CompareTo(a) >= 0 && b.CompareTo(c) > 0 || b.CompareTo(a) > 0 && b.CompareTo(c) >= 0)
+            {
+                return b;
+            }
+            else if(c.CompareTo(b) > 0 && c.CompareTo(a) > 0 || c.CompareTo(b) >= 0 && c.CompareTo(a) > 0 || c.CompareTo(b) > 0 && c.CompareTo(a) >= 0)
+            {
+                return c;
+            }
+            return a;
         }
     }
 }
