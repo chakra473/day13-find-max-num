c96d964346fba256a384e3432db96ea68d85d256
chakravarthy <m.chakra473@gmail.com>
2022-03-22 15:57:30 +0530

    iniated

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace day13_find_max
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to find the max number");
+        }
+    }
+}
