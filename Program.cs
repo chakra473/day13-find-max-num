9f29427f01c7cd3f645b48d5bf9c5d3281bab482
chakravarthy <m.chakra473@gmail.com>
2022-03-22 16:26:29 +0530

    added [find max double among three numbers] to uc2

--- Program.cs
+++ Program.cs
@@ -13,6 +13,14 @@ namespace day13_find_max
             int c=int.Parse(Console.ReadLine());
             Console.WriteLine("the maximum number among three number is :");
             Console.WriteLine(FindMax(a, b, c));
+            Console.WriteLine("enter three double numbers");
+            double d=double.Parse(Console.ReadLine());
+            double e=double.Parse(Console.ReadLine());
+            double f=double.Parse(Console.ReadLine());
+            Console.WriteLine("the maximum double number among three number is :");
+            Console.WriteLine(FindMaxDouble(d, e, f));
+
+
         }
         public static int FindMax(int a,int b,int c)
         {
@@ -25,6 +33,22 @@ namespace day13_find_max
                 return b;
             }
             else if(c.CompareTo(b) > 0 && c.CompareTo(a) > 0 || c.CompareTo(b) >= 0 && c.CompareTo(a) > 0 || c.CompareTo(b) > 0 && c.CompareTo(a) >= 0)
+            {
+                return c;
+            }
+            return a;
+        }
+        public static double FindMaxDouble(double a, double b, double c)
+        {
+            if (a.CompareTo(b) > 0 && a.CompareTo(c) > 0 || a.CompareTo(b) >= 0 && a.CompareTo(c) > 0 || a.CompareTo(b) > 0 && a.CompareTo(c) >= 0)
+            {
+                return a;
+            }
+            else if (b.CompareTo(a) > 0 && b.CompareTo(c) > 0 || b.CompareTo(a) >= 0 && b.CompareTo(c) > 0 || b.CompareTo(a) > 0 && b.CompareTo(c) >= 0)
+            {
+                return b;
+            }
+            else if (c.CompareTo(b) > 0 && c.CompareTo(a) > 0 || c.CompareTo(b) >= 0 && c.CompareTo(a) > 0 || c.CompareTo(b) > 0 && c.CompareTo(a) >= 0)
             {
                 return c;
             }
